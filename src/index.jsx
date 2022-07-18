bebc039edd35fd72ca45b3082cadc9e0da2ec7c5
minjae <minjae@hyundai-autoever.com>
2022-07-18 17:21:27 +0900

    add todo list

--- src/index.jsx
+++ src/index.jsx
@@ -2,34 +2,69 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function Todo({ value }) {
+  console.log("todo", value);
   return (
-    <div>
-      <p>{value}
+    <p>{value}
       <button type='button'> 완료 </button>
-      </p>
-    </div>
+    </p>
   )
 }
 
-function Page({ todos, inputTodoValue, }) {
-  
+function Input( {handleClick} ) {
+  console.log(handleClick);
+  return(
+    <form onSubmit={handleClick}>
+        <label>
+          <input type="text" onChange={handleClick} />
+        </label>
+        <input type="submit" value="추가" />
+      </form>
+  )
+}
+
+function Page({ todoList, handleClick, }) {
+  console.log("list", todoList, handleClick );
   return (
     <div>
       <h1>To-do</h1>
-      <p>할 일이 없어요!</p>
-      <Todo value="안녕" />
+      <Input 
+        onSubmit={handleClick}
+      />
+      { todoList.length > 0 ?
+        <p>
+          {todoList.map((todo, index) => 
+            <Todo value={(index+1) + ". " + todo} />
+          )}
+        </p>
+        : 
+        <p>
+          할 일이 없어요!
+        </p>
+      }
     </div>
   );
 }
 
 function App() {
   const [state, setState] = useState({
-    todos: [],
+    todoList: [],
   });
-  
+
+  const {todoList, } = state;
+
+  function handleClick(todo) {
+    setState((todoList) => ({
+      todoList: [...todoList, todo]
+    }))
+  }
+
   return (
-    <Page />
-  
+    <div>
+      <Page 
+        todoList={todoList}
+        onSubmit={(todo) => handleClick(todo)}
+      />
+    </div>
   );
 }
 
